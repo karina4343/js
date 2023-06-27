d6ffcb6cf871195707c01494ce09b3235673b829
karina manias <kari_4343@hotmail.com>
2023-06-27 19:51:43 -0300

    cambio

---
+++ libreria.js
@@ -0,0 +1,7 @@
+  Swal.fire({
+    title: 'BIENVENIDO A 101 PATITAS!',
+    text: '¡Le deceamos una excelente compra!',
+    /*icon: 'success',*/
+    confirmButtonText: 'Aceptar',
+    /*background : '#E3CAA5'*/
+  });
